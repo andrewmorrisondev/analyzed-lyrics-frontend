e33b8023b777d03998b4321eddd182bada52ef5e
Nick Wooten <nswooten@gmail.com>
2023-10-31 14:39:20 -0500

    lyrics are highlighting based on current position in the song and continues to work when paused and played again

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,44 +1,62 @@
-import SpotifyPlayer from "react-spotify-web-playback"
-import { useState, useEffect} from 'react'
+import SpotifyPlayer from "react-spotify-web-playback";
+import { useState, useEffect } from 'react';
 
 interface PlayerProps {
   accessToken: string;
   trackUri: string;
-  currentTime: number;
-  setCurrentTime: (newTime: number) => void;
+  setStartTime: (newTime: number) => void;
   isPlaying: boolean;
   setIsPlaying: (playing: boolean) => void;
+  setPausedTime: (pauseTime: number) => void;
+  pausedTime: number;
+  startTime: number;
+  totalElapsedTime: number;
+  setTotalElapsedTime: (time:number) => void;
 }
 
 export default function Player(props: PlayerProps) {
-  const {trackUri, accessToken, setCurrentTime, isPlaying, setIsPlaying } = props
-
+  const { trackUri, accessToken, setStartTime, isPlaying, setIsPlaying, setPausedTime, startTime, totalElapsedTime, setTotalElapsedTime } = props
   const [play, setPlay] = useState(false)
-  
 
-  const controlsElement = document.querySelector('div[data-component-name="Controls"]')
-  let dataPlaying = controlsElement?.getAttribute('data-playing')
+  const [currentSong, setCurrentSong] = useState('')
 
   useEffect(() => {
-    // Update dataPlaying when isPlaying prop changes
-    dataPlaying = isPlaying ? 'true' : 'false';
-  }, [play])
+    if (play && trackUri !== currentSong) {
+      // A new song is starting
+      setTotalElapsedTime(0)
+      setStartTime(Date.now())
+      setCurrentSong(trackUri)
+    } else if (!play && trackUri === currentSong) {
+      // The song is paused
+      setPausedTime(Date.now() - startTime)
+      // Accumulate elapsed time when the song is paused
+      setTotalElapsedTime(totalElapsedTime + (Date.now() - startTime))
+    } else if (play && trackUri === currentSong) {
+      // The song is resumed
+      setStartTime(Date.now())
+    }
+  }, [isPlaying, trackUri, currentSong, play])
 
-  // useEffect(() => setIsPlaying(!dataPlaying), [isPlaying])
   useEffect(() => setPlay(true), [trackUri])
 
   if (!accessToken) return null
 
-  return <SpotifyPlayer 
-            token={accessToken}
-            showSaveIcon
-            callback={state => {
-              if (!state.isPlaying) setPlay(false)
-              setCurrentTime(state.position * 1000)
-              // console.log(state)
-              setIsPlaying(state.isPlaying)
-            }}
-            play={play}
-            uris={trackUri ? [trackUri] : []}
-          />
+  return (
+    <SpotifyPlayer
+      token={accessToken}
+      showSaveIcon
+      callback={state => {
+        if (!state.isPlaying) {
+          setPlay(false)
+        }
+        if (state.isPlaying) {
+          setPlay(true)
+        }
+        setIsPlaying(state.isPlaying)
+      }}
+      play={play}
+      uris={trackUri ? [trackUri] : []}
+      initialVolume={0.1}
+    />
+  )
 }
